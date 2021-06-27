b0f805ed7429e8cf383fc128b0906c4328bbafe0
Chetan Talwar <thechetantalwar@users.noreply.github.com>
2021-06-27 11:41:46 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 app.get('/', function(request,result){
-    result.send('Hello World\n')
+    result.send('Thinknyx Technologies\n')
 })
 app.listen(8080,"0.0.0.0",function(){
     console.log('hello world app is listening on port 8080')
